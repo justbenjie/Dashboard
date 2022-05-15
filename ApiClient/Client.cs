50cd2194886b8366484a837ab7577c424a2d9921
justbenjie <filippoknest3@gmail.com>
2022-05-15 23:24:15 +0300

    added api client

---
+++ ApiClient/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ApiClient
+{
+    public static class Client
+    {
+        public static VacanciesInfo GetVacanciesInfo(string name)
+        {
+            string url = $"http://127.0.0.1:8000/vacancies/{name}";
+
+            //  Using web client
+            WebClient client = new WebClient();
+            string results = Encoding.UTF8.GetString(client.DownloadData(url));
+            VacanciesInfo vacanciesInfo = JsonConvert.DeserializeObject<VacanciesInfo>(results);
+            return vacanciesInfo;
+        }
+
+        }
+    }
---
+++ ApiClient/VacnciesInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class VacanciesInfo
+    {
+            public int Count { get; set; }
+            public Dictionary<string, int> Skills { get; set; }
+            public Dictionary<string, double> SalaryExp { get; set; }
+            public double SalaryMin { get; set; }
+            public double SalaryMax { get; set; }
+            public double SalaryMedian { get; set; }
+            public Dictionary<string, int> Schedule { get; set; }
+            public Dictionary<string, int> Experience { get; set; }
+    }
+}
