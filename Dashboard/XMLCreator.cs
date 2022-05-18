bd958581b652ba501165cf9956a34daf82aca769
justbenjie <filippoknest3@gmail.com>
2022-05-18 21:37:08 +0300

    added XMLfiles

---
+++ Dashboard/XMLCreator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Dashboard
+{
+    internal class XMLCreator
+    {
+        public static void SavaData(object obj, string filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType()); 
+            TextWriter writer = new StreamWriter(filename);
+            xmlSerializer.Serialize(writer, obj);
+            writer.Close();
+        }
+
+        public static SettingData ReadData(SettingData obj, string filename)
+        {
+            
+            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+            FileStream read = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            obj = (SettingData)xmlSerializer.Deserialize(read);
+            read.Close();
+            return obj;
+            
+        }
+    }
+}
